0368a954051499f5d1dd06533db27691ba1d7d1f
rusty-dev1302 <vaibhav.agochiya@gmail.com>
2023-08-30 20:17:55 -0800

    Adding sidebar changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,26 +6,6 @@ import { KeycloakService} from 'keycloak-angular';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit{
-
-  userName: String = "";
-  isAuthenticated: boolean = false;
-
-  constructor(private keycloakService: KeycloakService) { }
-
-  public async ngOnInit() {
-    this.isAuthenticated = await this.keycloakService.isLoggedIn();
-    this.getUserDetails();
-  }
-  
-  getUserDetails() {
-    if(this.isAuthenticated) {
-      this.userName = this.keycloakService.getUsername();
-    }
-  }
-
-  logout() {
-    this.keycloakService.logout();
-  }
+export class HeaderComponent {
 
 }
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
+import { KeycloakProfile } from 'keycloak-js';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit{
+
+  firstName: string = "";
+  lastName: string = "";
+  userProfile!: KeycloakProfile;
+  isAuthenticated: boolean = false;
+
+  constructor(private keycloakService: KeycloakService) { }
+
+  public async ngOnInit() {
+    this.isAuthenticated = await this.keycloakService.isLoggedIn();
+
+    this.loadUserProfile();
+  }
+
+  loadUserProfile() {
+    if(this.isAuthenticated) {
+      this.keycloakService.loadUserProfile().then(
+        (userProfile) => {
+            this.userProfile = userProfile;
+            this.firstName = this.userProfile.firstName!;
+            this.lastName = this.userProfile.lastName!;
+            console.log(this.userProfile)
+        }
+        );
+    }
+  }
+
+  logout() {
+    this.keycloakService.logout();
+  }
 
 }
