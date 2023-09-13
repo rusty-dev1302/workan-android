3704eb3922d443543ba28ccf8de7d677990ca06f
rusty-dev1302 <vaibhav.agochiya@gmail.com>
2023-09-13 16:04:23 +0530

    Misc changes

--- src/app/components/dashboard-order-details/dashboard-order-details.component.ts
+++ src/app/components/dashboard-order-details/dashboard-order-details.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationService } from 'src/app/services/navigation.service';
 
 @Component({
   selector: 'app-dashboard-order-details',
   templateUrl: './dashboard-order-details.component.html',
   styleUrls: ['./dashboard-order-details.component.css']
 })
-export class DashboardOrderDetailsComponent {
+export class DashboardOrderDetailsComponent implements OnInit {
+
+  constructor(
+  ) { }
+
+  ngOnInit() {
+  }
 
 }
