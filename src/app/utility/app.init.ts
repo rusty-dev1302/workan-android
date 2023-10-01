f50b1368256177ca86b059ecf15138fa03e699c3
rusty-dev1302 <vaibhav.agochiya@gmail.com>
2023-10-01 13:17:51 +0530

    Misc Changes

--- src/app/utility/app.init.ts
+++ src/app/utility/app.init.ts
@@ -1,10 +1,11 @@
 import { KeycloakService } from "keycloak-angular";
+import { environment } from "src/environments/environment";
 
 export function initializeKeycloak(keycloak: KeycloakService): () => Promise<boolean> {
     return () =>
         keycloak.init({
             config: {
-                url: 'http://localhost:8080/auth',
+                url: environment.keycloak.url,
                 realm: 'workan',
                 clientId: 'workan-app'
             },
