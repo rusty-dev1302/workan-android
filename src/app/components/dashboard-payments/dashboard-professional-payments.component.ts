aaaa22a5a41a9785541cd4a715e9e5cc77a26236
rusty-dev1302 <vaibhav.agochiya@gmail.com>
2023-10-27 13:18:51 -0700

    Payments Changes

--- src/app/components/dashboard-payments/dashboard-professional-payments.component.ts
+++ src/app/components/dashboard-payments/dashboard-professional-payments.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { NavigationService } from 'src/app/services/navigation.service';
 
 @Component({
-  selector: 'app-dashboard-professional-payments',
-  templateUrl: './dashboard-professional-payments.component.html',
-  styleUrls: ['./dashboard-professional-payments.component.css']
+  selector: 'app-dashboard-payments',
+  templateUrl: './dashboard-payments.component.html',
+  styleUrls: ['./dashboard-payments.component.css']
 })
-export class DashboardProfessionalPaymentsComponent implements OnInit {
+export class DashboardPaymentsComponent implements OnInit {
 
   constructor(
     private navigationService: NavigationService
