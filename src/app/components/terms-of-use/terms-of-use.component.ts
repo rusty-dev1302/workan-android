2fa5b819aeb3cf6d34ad4d0653737caaa63ba47d
rusty-dev1302 <vaibhav.agochiya@gmail.com>
2023-11-22 22:29:18 +0530

    Misc changes

--- src/app/components/terms-of-use/terms-of-use.component.ts
+++ src/app/components/terms-of-use/terms-of-use.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationService } from 'src/app/services/navigation.service';
 
 @Component({
   selector: 'app-terms-of-use',
   templateUrl: './terms-of-use.component.html',
   styleUrls: ['./terms-of-use.component.css']
 })
-export class TermsOfUseComponent {
+export class TermsOfUseComponent implements OnInit{
+  constructor(
+    private navigationService: NavigationService
+  ) {
 
+  }
+
+  ngOnInit(): void {
+    this.navigationService.pageLoaded();
+  }
 }
