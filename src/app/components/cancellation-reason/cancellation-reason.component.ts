9e65656f98cc87e0657c0f3eb6709f5efb652b8a
rusty-dev1302 <vaibhav.agochiya@gmail.com>
2024-03-10 10:01:27 -0700

    Misc changes

---
+++ src/app/components/cancellation-reason/cancellation-reason.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cancellation-reason',
+  templateUrl: './cancellation-reason.component.html',
+  styleUrls: ['./cancellation-reason.component.css']
+})
+export class CancellationReasonComponent {
+
+}
