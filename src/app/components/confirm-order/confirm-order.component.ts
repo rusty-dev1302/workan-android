1d96cae1984a2ae652d3b649a021e128cc03101f
rusty-dev1302 <vaibhav.agochiya@gmail.com>
2024-03-25 21:21:25 -0700

    Misc changes

--- src/app/components/confirm-order/confirm-order.component.ts
+++ src/app/components/confirm-order/confirm-order.component.ts
@@ -1,9 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { CreateOrderRequest } from 'src/app/common/create-order-request';
 import { constants } from 'src/environments/constants';
 import { Customer } from 'src/app/common/customer';
 import { SlotTemplateItem } from 'src/app/common/slot-template-item';
-import { ContactDetail } from 'src/app/common/contact-detail';
 import { ListingService } from 'src/app/services/listing.service';
 import { OrderService } from 'src/app/services/order.service';
 import { KeycloakService } from 'keycloak-angular';
@@ -28,7 +26,6 @@ export class ConfirmOrderComponent implements OnInit {
 
   currentDate!: string;
   currentSlots!: any[];
-  availableMenuItems!: any[];
   totalMenuCharges: number = 0;
   currentStep: number = 0;
 
@@ -40,7 +37,6 @@ export class ConfirmOrderComponent implements OnInit {
   dayBoolArray: boolean[] = JSON.parse(JSON.stringify(constants.DAY_BOOL_ARRAY_INIT));
 
   @Input() listingId: number = 0;
-  customerAddress!: ContactDetail;
 
   constructor(
     private datePipe: DatePipe,
@@ -56,7 +52,6 @@ export class ConfirmOrderComponent implements OnInit {
 
   ngOnInit(): void {
     this.getSlotsForDay(new Date(), 0);
-    this.loadCustomer();
   }
 
   slotDate(index: number): Date {
@@ -64,16 +59,6 @@ export class ConfirmOrderComponent implements OnInit {
     date.setDate(date.getDate() + index);
 
     return date;
-  }
-
-  loadCustomer() {
-    const sub = this.userService.getUserByEmail(this.keycloakService.getUsername()).subscribe(
-      (data) => {
-        this.customerAddress = data.contact;
-        sub.unsubscribe();
-      }
-    );
-
   }
 
   getSlotsForDay(date: Date, i: number) {
@@ -122,15 +107,6 @@ export class ConfirmOrderComponent implements OnInit {
     );
   }
 
-  loadServicePricings() {
-    const sub = this.listingService.getServicePricings(this.listingId).subscribe(
-      (response) => {
-        this.availableMenuItems = response;
-        this.selectedMenuItems = JSON.parse(JSON.stringify(this.availableMenuItems));
-        sub.unsubscribe();
-      }
-    );
-  }
 
   addMenuItem(i: number) {
     if(this.selectedMenuItems[i].quantity==null) {
@@ -159,9 +135,6 @@ export class ConfirmOrderComponent implements OnInit {
 
   nextStep() {
     this.currentStep++;
-    if(this.currentStep==1) {
-      this.loadServicePricings();
-    }
   }
 
   previousStep() {
