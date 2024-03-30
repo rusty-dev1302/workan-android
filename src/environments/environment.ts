3db60302da3deee95b9bc143ff2f7dd216c987b4
rusty-dev1302 <vaibhav.agochiya@gmail.com>
2024-03-29 17:25:04 -0700

    Misc changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: false,
     keycloak : {
-        // url: 'https://www.workan.ca/auth',
-        // issuer: "https://www.workan.ca/auth/realms/workan",
-        // redirectUri: "https://www.workan.ca/",
+        // url: 'https://workan.ca/auth',
+        // issuer: "https://workan.ca/auth/realms/workan",
+        // redirectUri: "https://workan.ca/",
         url: 'http://localhost:8080/auth',
         issuer: "http://localhost:8080/auth/realms/workan",
         redirectUri: "http://localhost:4200/",
