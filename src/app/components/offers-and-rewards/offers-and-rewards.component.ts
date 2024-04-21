5f9955a0203abb2707ea801f411ea93638e0f9c8
rusty-dev1302 <vaibhav.agochiya@gmail.com>
2024-04-21 09:31:21 -0700

    Misc Changes

--- src/app/components/offers-and-rewards/offers-and-rewards.component.ts
+++ src/app/components/offers-and-rewards/offers-and-rewards.component.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { NavigationService } from 'src/app/services/navigation.service';
 import { UserService } from 'src/app/services/user.service';
 import { KeycloakService } from 'keycloak-angular';
+import { constants } from 'src/environments/constants';
+import { Customer } from 'src/app/common/customer';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-offers-and-rewards',
@@ -13,13 +16,14 @@ import { KeycloakService } from 'keycloak-angular';
 })
 export class OffersAndRewardsComponent implements OnInit{
 
-  user!: any;
+  user!: Customer;
+  referralCode!: any;
 
   constructor(
     private navigationService: NavigationService,
     private userService: UserService,
-    private keycloakService: KeycloakService
-
+    private keycloakService: KeycloakService,
+    private toastr: ToastrService
   ) {
 
   }
@@ -28,12 +32,41 @@ export class OffersAndRewardsComponent implements OnInit{
     const sub = this.userService.getUserShortByEmail(this.keycloakService.getUsername()).subscribe(
       (user) => {
         this.user = user;
-        
         this.navigationService.pageLoaded();
+        if(!this.user.professional) {
+          this.getReferralCodeInfo();
+        }
         sub.unsubscribe();
       }
     );
-    
+  
+  }
+
+  copyCode() {
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = this.referralCode.code;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+    this.toastr.info("Copied to Clipboard")
+  }
+
+  getReferralCodeInfo() {
+    const sub = this.userService.getReferralCodeInfo().subscribe(
+      (code) => {
+        if(code.state!=constants.ERROR_STATE) {
+          this.referralCode = code;
+        }
+
+        sub.unsubscribe();
+      }
+    );
   }
 
 
